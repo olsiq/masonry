c5da0ef96866d9a1066d7b1aaaaca408cf7347d7
olsiq <64708189+olsiq@users.noreply.github.com>
2022-04-26 01:32:35 +0300

    IMPROVEMENT

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./components/counter/Counter";
+
+import { useSelector, useDispatch } from "react-redux";
+import {
+  fetchFromApi,
+  selectUrls,
+  selectAuthors,
+  selectIds,
+} from "./app/counterSlice";
+import styles from "./components/card/card.module.css";
+// import styles from "./components/counter/counter.module.css";
+// import { Counter } from "./components/counter/Counter";
+import { Layout } from "./components/ui";
+import { Card } from "./components/card";
 import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+  const urls = useSelector(selectUrls);
+  const authors = useSelector(selectAuthors);
+  const ids = useSelector(selectIds);
+
+  const sizes = ["small", "medium", "large"];
+  const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+  const cards = urls.map((url, index) => (
+    <Card
+      key={ids[index]}
+      author={authors[index]}
+      size={sizes[random(0, 3)]}
+      title={`art ${ids[index]}`}
+      image={url}
+      alt="miaou"
+    />
+  ));
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+        {/* <Counter /> */}
+        <button
+          className={styles.asyncButton}
+          onClick={() =>
+            dispatch(fetchFromApi("https://picsum.photos/v2/list"))
+          }
+        >
+          get data
+        </button>
+        <Layout>{cards}</Layout>
       </header>
     </div>
   );
--- src/components/card/content/Content.js
+++ src/components/card/content/Content.js
@@ -1,25 +1,17 @@
 import React, { Fragment } from "react";
 
-import "../../images/astro.jpg";
-import "../../images/blue.jpg";
-import "../../images/dear.jpg";
-import "../../images/mr-robot.jpg";
-import "../../images/mr-robot-1.jpg";
-import "../../images/psyco.jpg";
-import "../../images/pubg.jpg";
-import "../../images/skull.jpg";
-
 export const Content = ({ image, alt, size }) => {
-  const img = require(`../../images/${image}`);
+  const img = image;
 
   const dimension = {
     width: "100%",
     height: "90%",
     borderRadius: "10px 10px 0 0 ",
   };
+  const path = image;
   return (
     <Fragment>
-      <img alt={alt} src={img} style={dimension} />
+      <img alt={alt} src={path} style={dimension} />
     </Fragment>
   );
 };
