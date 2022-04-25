7dd42a0c867edeeb0719fb87e1d2580dd699a904
olsiq <64708189+olsiq@users.noreply.github.com>
2022-04-25 23:14:08 +0300

    initial

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,67 +1,35 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+  fetchFromApi,
+  selectUrls,
+  selectAuthors,
+  selectIds,
+} from "./counterSlice";
+import styles from "./Counter.module.css";
 
 export function Counter() {
-  const count = useSelector(selectCount);
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const urls = useSelector(selectUrls);
+  const authors = useSelector(selectAuthors);
+  const ids = useSelector(selectIds);
 
-  const incrementValue = Number(incrementAmount) || 0;
+  console.log(urls, authors, ids);
+
+  const list = urls.map((url, index) => (
+    <li key={ids[index]}>{authors[index]}</li>
+  ));
 
   return (
     <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
+      <button
+        className={styles.asyncButton}
+        onClick={() => dispatch(fetchFromApi("https://picsum.photos/v2/list"))}
+      >
+        get data
+      </button>
+
+      <ul>{list}</ul>
     </div>
   );
 }
